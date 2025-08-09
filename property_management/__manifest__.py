e051936d8908d0d67891f6649152cdb905eaf100
Abeza Getachew <94830400+abeza5389getachew@users.noreply.github.com>
2025-08-09 11:26:48 +0300

    Update __manifest__.py

--- property_management/__manifest__.py
+++ property_management/__manifest__.py
@@ -1,27 +1,26 @@
 {
-    "name": "property management",
+    "name": "Property Management",
     "author": "Abeza Getachew",
     "license": "LGPL-3",
-    'summary': "module for property management",
-    'category': 'Property',
-    'description': 'A module to manage properties',
+    "summary": "Module for property and rental management",
+    "category": "Real Estate",
+    "description": "A complete solution to manage properties, leases, tenants, and rent payments in Odoo.",
     "version": "18.0.1.0",
-    'depends': ['base', 'web'],
-    #'depends': ['base', 'web', 'report_xlsx'],
-
-    'sequence': 0,
+    "depends": ["base", "web"],
+    "sequence": 0,
     "data": [
-       # "security/security.xml",
-        "security/ir.model.access.csv",
-        "views/rent_payment_view.xml",
-        "views/lease_view.xml",
-        "views/tenant_view.xml",
-        "views/property_views.xml",
-        "views/menu.xml",
-       # "report/lease_report.xml"
+         "data/cron.xml",
+         "security/security.xml",
+         "security/ir.model.access.csv",
+         "views/property_views.xml",
+         "views/tenant_views.xml",
+         "views/lease_views.xml",
+         "views/rent_payment_views.xml",
+         "views/menu.xml",
+         "views/lease_report.xml",
+         "views/lease_report_templates.xml"
     ],
-    'installable': True,
-    'auto_install': False,
-    'application': True,
-    
+    "installable": True,
+    "auto_install": False,
+    "application": True
 }
